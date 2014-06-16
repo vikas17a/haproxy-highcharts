b96956166e41af9aace344be51faaff1c86dfcb4
vikas17a <vikas17a@gmail.com>
2014-06-16 10:44:19 +0530

    new version updated dynamic update

--- haproxy-stats/app.js
+++ haproxy-stats/app.js
@@ -16,6 +16,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/stats', function(err, database) {
 });
 var responseFile = "prod-haproxy";
 var responseFile2 = "api-haproxy";
+
 var urlRequest = "http://healthkart:adw38&6cdQE@healthkart.com/haproxy?stats;csv;norefresh";
 var urlRequest2 = "http://healthkart:adw38&6cdQE@api.healthkart.com/haproxy?stats;csv;norefresh";
 
@@ -53,85 +54,94 @@ function getStats(cluster, urlRequest, responseFile) {
 	}, 10000);
 }
 
+/**** Parsing of csv to json ***************/
+
+function CSVToArray(strData, strDelimiter) {
+	// Check to see if the delimiter is defined. If not,
+	// then default to comma.
+	strDelimiter = (strDelimiter || ",");
+	// Create a regular expression to parse the CSV values.
+	var objPattern = new RegExp((
+	// Delimiters.
+	"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+	// Quoted fields.
+	"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+	// Standard fields.
+	"([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+	// Create an array to hold our data. Give the array
+	// a default empty first row.
+	var arrData = [ [] ];
+	// Create an array to hold our individual pattern
+	// matching groups.
+	var arrMatches = null;
+	// Keep looping over the regular expression matches
+	// until we can no longer find a match.
+	while (arrMatches = objPattern.exec(strData)) {
+		// Get the delimiter that was found.
+		var strMatchedDelimiter = arrMatches[1];
+		// Check to see if the given delimiter has a length
+		// (is not the start of string) and if it matches
+		// field delimiter. If id does not, then we know
+		// that this delimiter is a row delimiter.
+		if (strMatchedDelimiter.length
+				&& (strMatchedDelimiter != strDelimiter)) {
+			// Since we have reached a new row of data,
+			// add an empty row to our data array.
+			arrData.push([]);
+		}
+		// Now that we have our delimiter out of the way,
+		// let's check to see which kind of value we
+		// captured (quoted or unquoted).
+		if (arrMatches[2]) {
+			// We found a quoted value. When we capture
+			// this value, unescape any double quotes.
+			var strMatchedValue = arrMatches[2].replace(new RegExp("\"\"",
+					"g"), "\"");
+		} else {
+			// We found a non-quoted value.
+			var strMatchedValue = arrMatches[3];
+		}
+		// Now that we have our value string, let's add
+		// it to the data array.
+		arrData[arrData.length - 1].push(strMatchedValue);
+	}
+	// Return the parsed data.
+	return (arrData);
+}
+
+function CSV2JSON(csv) {
+	var array = CSVToArray(csv);
+	var objArray = [];
+	for (var i = 1; i < array.length; i++) {
+		objArray[i - 1] = {};
+		for (var k = 0; k < array[0].length && k < array[i].length; k++) {
+			var key = array[0][k];
+			objArray[i - 1][key] = array[i][k]
+		}
+	}
+
+	var json = JSON.stringify(objArray);
+	var str = json.replace(/},/g, "},\r\n");
+
+	eventEmmit.emit('json_parsed');
+	return str;
+}
+
+/*******Parsing of csv to json****************/
+
 function parsingJson(cluster, responseFile) {
-	function CSVToArray(strData, strDelimiter) {
-		// Check to see if the delimiter is defined. If not,
-		// then default to comma.
-		strDelimiter = (strDelimiter || ",");
-		// Create a regular expression to parse the CSV values.
-		var objPattern = new RegExp((
-		// Delimiters.
-		"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
-		// Quoted fields.
-		"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
-		// Standard fields.
-		"([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
-		// Create an array to hold our data. Give the array
-		// a default empty first row.
-		var arrData = [ [] ];
-		// Create an array to hold our individual pattern
-		// matching groups.
-		var arrMatches = null;
-		// Keep looping over the regular expression matches
-		// until we can no longer find a match.
-		while (arrMatches = objPattern.exec(strData)) {
-			// Get the delimiter that was found.
-			var strMatchedDelimiter = arrMatches[1];
-			// Check to see if the given delimiter has a length
-			// (is not the start of string) and if it matches
-			// field delimiter. If id does not, then we know
-			// that this delimiter is a row delimiter.
-			if (strMatchedDelimiter.length
-					&& (strMatchedDelimiter != strDelimiter)) {
-				// Since we have reached a new row of data,
-				// add an empty row to our data array.
-				arrData.push([]);
-			}
-			// Now that we have our delimiter out of the way,
-			// let's check to see which kind of value we
-			// captured (quoted or unquoted).
-			if (arrMatches[2]) {
-				// We found a quoted value. When we capture
-				// this value, unescape any double quotes.
-				var strMatchedValue = arrMatches[2].replace(new RegExp("\"\"",
-						"g"), "\"");
-			} else {
-				// We found a non-quoted value.
-				var strMatchedValue = arrMatches[3];
-			}
-			// Now that we have our value string, let's add
-			// it to the data array.
-			arrData[arrData.length - 1].push(strMatchedValue);
-		}
-		// Return the parsed data.
-		return (arrData);
-	}
-
-	function CSV2JSON(csv) {
-		var array = CSVToArray(csv);
-		var objArray = [];
-		for (var i = 1; i < array.length; i++) {
-			objArray[i - 1] = {};
-			for (var k = 0; k < array[0].length && k < array[i].length; k++) {
-				var key = array[0][k];
-				objArray[i - 1][key] = array[i][k]
-			}
-		}
-
-		var json = JSON.stringify(objArray);
-		var str = json.replace(/},/g, "},\r\n");
-
-		eventEmmit.emit('json_parsed');
-		return str;
-	}
-
+	
 	fs.readFile(responseFile, 'utf8', function(err, data) {
 		if (err) {
 			return console.log(err);
 		}
-		var myObj = CSV2JSON(data);
+		try{
+			var myObj = CSV2JSON(data);
+		}
+		catch(err){
+			console.log(new Date() + ' ERROR : ' + err);
+		}
 		var jsonObj = eval('(' + myObj + ')');
-
 		eventEmmit.on('json_parsed', addTodb);
 		var index = 0;
 		function addTodb() {
@@ -183,10 +193,7 @@ app.get('/', function(req, res) {
 	res.render("index.html");
 });
 
-app.get('/rest/api/', function(req, res) {
-	console.log(req.query['pxname']);
-	console.log(req.query['box']);
-	console.log(req.query['svname']);
+app.get('/rest/load/', function(req, res) {
 	var svname = req.query['svname'];
 	var collection_name = req.query['box'] + '_' + req.query['pxname'];
 	var collection = db.collection(collection_name);
@@ -202,6 +209,33 @@ app.get('/rest/api/', function(req, res) {
 			obj["data"].push([date, rate]);
 		}
 		res.send(obj);
+	});
+});
+
+app.get('/rest/live/', function(req,res){
+	console.log('request recived live');
+	var svname = req.query['svname'];
+	var requestFile = req.query['box'] + "-haproxy";
+	fs.readFile(requestFile, 'utf8', function(err, data) {
+		var jsonObj;
+		if (err) {
+			return console.log(err);
+		}
+		try{
+			var myObj = CSV2JSON(data);
+			jsonObj = eval('(' + myObj + ')');
+			var rendObj = {};
+			for(var index in jsonObj){
+				if(jsonObj[index]['# pxname'] == req.query['pxname']){
+					rendObj['timestamp'] = Date.now();
+					rendObj['rate'] = parseInt(jsonObj[index]['rate']);
+				}
+			}
+			res.send(rendObj);
+		}
+		catch(err){
+			console.log(new Date() + ' ERROR : ' + err);
+		}
 	});
 });
 
